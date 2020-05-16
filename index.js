475fa1775461b27ca29b7d8561d0d4125aa89dc9
Amelia Haba <ameliahaba347@gmail.com>
2020-05-16 22:47:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ return array;
 }
 
 function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
         newFacts.push(facts[i] +"!!!");
         i++;
     }
